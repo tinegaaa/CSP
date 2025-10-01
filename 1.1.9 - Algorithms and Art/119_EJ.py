c745b9ad49ad8ee9e76026df568bfee0f8174bae
tinegaaa <306017@shakopeeschools.org>
2025-10-01 08:56:07 -0500

    Update 119_EJ.py

--- 1.1.9 - Algorithms and Art/119_EJ.py
+++ 1.1.9 - Algorithms and Art/119_EJ.py
@@ -7,21 +7,36 @@ trtl.addshape("football", ((5,0),(5,3),(4,5),(3,7),(1,9),(0,9),(-1,9),(-3,7),(-4
 turtle_shapes = ["square", "square", "circle", "square", "square", "e","football"]
 
 #Add the football
-fb = trtl(shape="football")
+fb = trtl.Turtle(shape = "football")
 fb.setheading(90)
+startx = -100 
+starty = 0
+
+line = []
+for i in range(5):
+    var = ("lineman" + str(i))
+    if i == 2: 
+        tshape = "circle"
+    else:
+        tshape = "square"
+
+    var = trtl.Turtle(shape = tshape)
+    var.penup()
+    var.goto(startx, starty)
+    startx += 50
+    line.append(var)
+print(line)
+
+
+
 #Add the question for which play to run
+#dive = trtl.textinput("Chose Play","12 or 13?")
+#if dive == "12":
+ 
+ 
+ 
+
 #Have the linemen block down
 #Have the B-back run thru the gap with the ball
-#Touchdown celebration 
-
-
-
-
-
-
-
-
-
-
 wn = trtl.Screen()
 wn.mainloop()
