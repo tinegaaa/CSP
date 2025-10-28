289ffe9f73089cda6cf93907ff2ec9ec7b6f98cf
tinegaaa <306017@shakopeeschools.org>
2025-10-28 08:38:34 -0500

    Update 125_EJ.py

---
+++ 1.2.5 - Shall We Play a Game/125_EJ.py
@@ -0,0 +1,30 @@
+import turtle as trtl
+trtl.addshape("hanger",((0,0),(0,7),(-6,2),(0,-7),(-4,7),(4,-7)))
+hanger = trtl.Turtle(shape = "hanger")
+hanger.color("black")
+# TODO Starting screen with the hanger, letters, and the lines that show how letters  lines there are and where they will go.
+# TODO Have a number in the corner that shows how many guesses there is left 
+# TODO Have it where it makes a body part everytime they get a guess wrong 
+# TODO If they get all the guesses have a winning screen at the end 
+# TODO If they don't get all the guesses have a losing screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
