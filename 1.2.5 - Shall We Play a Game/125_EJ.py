3d74b2e15d122cb3f7a0c926498ad5a9f0ecd5fb
tinegaaa <306017@shakopeeschools.org>
2025-10-28 09:09:30 -0500

    Update 125_EJ.py

--- 1.2.5 - Shall We Play a Game/125_EJ.py
+++ 1.2.5 - Shall We Play a Game/125_EJ.py
@@ -1,7 +1,21 @@
 import turtle as trtl
-trtl.addshape("hanger",((0,0),(0,7),(-6,7),(-6,2),(-5,2),(-5,6),(-1,6),(0,-7),(-2,-6),(-4,-6),(-4,-7),(5,-7),(5,-6),(0,-6)))
+import random
+
+
+word_list = ["apple", "banana", "orange", "grape", "strawberry"]
+chosen_word = random.choice(word_list)
+display = ["_"] * len(chosen_word)
+lives = 6
+guessed_letters = []
+
+print("Welcome to Hangman!")
+
+
+trtl.addshape("hanger",((0,0),(0,7),(-6,7),(-6,2),(-5,2),(-5,6),(-1,6),(-1,-6),(-2,-6),(-4,-6),(-4,-7),(5,-7),(5,-6),(0,-6)))
 hanger = trtl.Turtle(shape = "hanger")
 hanger.color("black")
+hanger.setheading(90)
+hanger.shapesize(20)
 # TODO Starting screen with the hanger, letters, and the lines that show how letters  lines there are and where they will go.
 # TODO Have a number in the corner that shows how many guesses there is left 
 # TODO Have it where it makes a body part everytime they get a guess wrong 
