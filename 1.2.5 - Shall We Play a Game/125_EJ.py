7537a5104647d7901ccfc87ab2c06b45577e6178
tinegaaa <306017@shakopeeschools.org>
2025-11-04 08:42:08 -0600

    Update 125_EJ.py

--- 1.2.5 - Shall We Play a Game/125_EJ.py
+++ 1.2.5 - Shall We Play a Game/125_EJ.py
@@ -2,14 +2,23 @@ import turtle as trtl
 import random
 
 
-trtl.addshape("hanger",((0,0),(0,7),(-6,7),(-6,2),(-5,2),(-5,6),(-1,6),(-1,-6),(-2,-6),(-4,-6),(-4,-7),(5,-7),(5,-6),(0,-6)))
-hanger = trtl.Turtle(shape = "hanger")
-hanger.color("black")
-hanger.setheading(90)
-hanger.shapesize(15)
+wn = trtl.Screen()
+trtl.write("guessing Game")
+trtl.hideturtle()
+wn.bgcolor("IndianRed1")
+
+
+EJ = trtl.Turtle()
+EJ.hideturtle()
+EJ.penup()
+EJ.goto(0, 100)
+
+def write_message(message):
+    EJ.write(message, align="center", font=("Arial", 18,))
+
 
 def hangman():
-    word_list = ["alden", "sid", "nehemiah", "tanush", "ej", "sammi"]
+    word_list = ["alden", "sid", "nehemiah", "tanush", "ej"]
     chosen_word = random.choice(word_list).lower()
     word_length = len(chosen_word)
     display = ["_"] * word_length
@@ -17,43 +26,43 @@ def hangman():
     guessed_letters = []
 
 
+    wn.textinput("start Game", "press OK to start.")
 
-trtl.write("Welcome to Hangman!")
+    while True:
+      
+        guess = wn.textinput("hello bro", f"Word: {"".join(display)} Lives: {lives} guess the letter bro:").lower()
+
+        if not guess:  
+            write_message("game over")
+            break
+
+        if guess in guessed_letters:
+            wn.textinput("dude cmon", f"yo fr? u guessed this already")
+            continue
+
+        guessed_letters.append(guess)
+        found_letter = False
+
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+                found_letter = True
+                
+        if not found_letter:
+            lives -= 1
+            wn.textinput("wrong bro", f"'{guess}' is wrong brotato \nLives remaining: {lives}")
+
+       
+        write_message(" ".join(display))
+
+        if "_" not in display:
+            write_message(f"you got lucky brochacho!  '{chosen_word}'.")
+            break
+        elif lives == 0:
+            write_message(f"bro for real? it was '{chosen_word}'.")
+            break
 
 
-
-
-
-
-
-
-
-
-
-
-
-# TODO Starting screen with the hanger, letters, and the lines that show how letters  lines there are and where they will go.
-# TODO Have a number in the corner that shows how many guesses there is left 
-# TODO Have it where it makes a body part everytime they get a guess wrong 
-# TODO If they get all the guesses have a winning screen at the end 
-# TODO If they don't get all the guesses have a losing screen
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-wn = trtl.Screen()
+hangman()
 wn.mainloop()
